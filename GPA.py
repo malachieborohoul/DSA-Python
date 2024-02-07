5da5e29115dac3aa4bd9f963315a689a9def53af
malachieborohoul <malachieborohoul@gmail.com>
2024-02-07 23:57:42 +0100

    GPA

--- GPA.py
+++ GPA.py
@@ -24,4 +24,5 @@ if num_courses > 0:
     print('Your GPA is {0}'.format(num_courses/num_total))
     print('Your GPA is {0}'.format(num_courses/num_total))
     print('Your GPA is {0}'.format(num_courses/num_total))
+    print('Your GPA is {0}'.format(num_courses/num_total))
 
