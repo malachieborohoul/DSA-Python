734eec807b61ceb0b539354082079ab964242e57
malachieborohoul <malachieborohoul@gmail.com>
2024-02-17 23:10:30 +0100

    iterable

--- lists.py
+++ lists.py
@@ -1,18 +1,18 @@
 fruits=["mango", "banana", "grape"]
 
-print("Enter a fruit name")
-done = True
+# print("Enter a fruit name")
+# done = True
 
-while done:
-    entry = input()
-    if entry == " ":
-        done = False
-    elif entry not in fruits:
-        fruits.append(entry)
-    else:
-        for fruit in fruits:
-            if fruit == entry:
-                print('{0} is already in the list'.format(fruit))        
-print(fruits)
+# while done:
+#     entry = input()
+#     if entry == " ":
+#         done = False
+#     elif entry not in fruits:
+#         fruits.append(entry)
+#     else:
+#         for fruit in fruits:
+#             if fruit == entry:
+#                 print('{0} is already in the list'.format(fruit))        
+# print(fruits)
 
-
+print(list(fruits))
